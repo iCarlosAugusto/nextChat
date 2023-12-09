a9aca434ff4985cd7178cc643a68a0d9543d7608
augusto.dev@proton.me <augusto.dev@proton.me>
2023-12-09 20:14:42 -0300

    Done basic of login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,14 +3,25 @@
 import { useState } from "react";
 import { InputComponent } from "../../components/Input";
 import { ButtonComponent } from "@/components/Button";
+import Image from 'next/image';
+import GoogleIcon from "../../../public/google.svg";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 
 export default function Login() {
-
+  
+  const [user] = useAuthState(auth);
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
 
   const handleAuthenticate = () => {
 
+  }
+
+  const handleAuthenticateGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
   }
 
   return (
@@ -21,6 +32,7 @@ export default function Login() {
 
       <InputComponent value={userPassword} onChange={e => setUserPassword(e.target.value)}/>
 
+      <Image src={GoogleIcon} alt="Google Login" onClick={handleAuthenticateGoogle} className="cursor-pointer"/>
       <ButtonComponent label="Entrar" onClick={handleAuthenticate}/>
     </main>
   );
