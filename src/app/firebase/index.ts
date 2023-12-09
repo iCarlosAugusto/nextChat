122f99d1f3015f59423ea72ed7ba4d9cd82a5952
augusto.dev@proton.me <augusto.dev@proton.me>
2023-12-09 19:02:43 -0300

    firebase configuration

---
+++ src/app/firebase/index.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyArDixHY3vhRaD1mLl5OlZ3WFd8ZdLi9jU",
+  authDomain: "nextchat-f3807.firebaseapp.com",
+  projectId: "nextchat-f3807",
+  storageBucket: "nextchat-f3807.appspot.com",
+  messagingSenderId: "66460463004",
+  appId: "1:66460463004:web:394cc2185e4a8166452259",
+  measurementId: "G-7ZSBGJF1PF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
