78f186917be6a06b62e79e7db182cc88aa4401ce
augusto.dev@proton.me <augusto.dev@proton.me>
2023-12-11 01:35:21 -0300

    feat: :sparkles: chat apge

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { ButtonComponent } from "@/components/Button";
+import { auth } from "@/firebase";
+import { useRouter } from "next/navigation";
+
 export default function ChatPage() {
+  const router = useRouter();
+
+  const signOut = async () => {
+    await auth.signOut();
+    router.push("/login");
+  };
+
   return <>
+    <ButtonComponent label="Fazer logout" onClick={signOut}/>
     <h1>This is the chat!</h1>
   </>
 }
